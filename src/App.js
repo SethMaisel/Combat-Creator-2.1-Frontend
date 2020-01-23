e91f5efee1d64b4d4ea39ebc8120272ebea529c8
github username <github email address>
2020-01-22 17:29:15 -0700

    auth still needs token

--- src/App.js
+++ src/App.js
@@ -6,11 +6,46 @@ import Header from "./components/Header";
 import "./App.css";
 
 class App extends Component {
+
+  state={
+    user: null
+  }
+
+  login(event) {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        body: JSON.stringify({
+            name: formData.get("name"),
+            email: formData.get("email"),
+            username: formData.get("username"),
+            password: formData.get("password"),
+        })
+    }).then(response => response.json())
+      .then()=> fetch("localhost:3000/login"))
+        .then(({ token }) => {
+            localStorage.setItem("token", token)
+        })
+}
+
+logout(event) {
+    event.preventDefault()
+    localStorage.removeItem("token")
+}
   render() {
     return (
+      
       <div className="App">
+        {this.state.user ?(
+        <>
         <Header />
         <FightPage />
+        </>)
+        : <Login 
+            login={this.login}
+            logout={this.logout}
+            />} 
       </div>
     );
   }
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,37 +2,22 @@ import React, { Component } from "react"
 
 export default class Login extends Component {
 
-    login(event) {
-        event.preventDefault()
-        const formData = new FormData(event.target)
-        fetch("http://localhost:3000/users", {
-            method: "POST",
-            body: JSON.stringify({
-                
-                username: formData.get("username"),
-                password: formData.get("password"),
-            })
-        }).then(response => response.json())
-            .then(({ token }) => {
-                localStorage.setItem("token", token)
-            })
-    }
-
-    logout(event) {
-        event.preventDefault()
-        localStorage.removeItem("token")
-    }
+    
     render() {
         return (
             <div>
-                <form onSubmit={this.login}>
+                <form onSubmit={this.props.login}>
+                    <label>Name</label>
+                    <input type="text" name="name" placeholder="Name" />
+                    <label>Email</label>
+                    <input type="text" name="email" placeholder="Email" />
                     <label>Username</label>
-                    <input type="text" name="username" />
+                    <input type="text" name="username" placeholder="UserName" />
                     <label>Password</label>
-                    <input type="password" name="password" />
+                    <input type="password" name="password" placeholder="Password" />
                     <input type="submit" value="Login" />
                 </form>
-                <form onSubmit={this.logout}>
+                <form onSubmit={this.props.logout}>
                     <input type="submit" value="Logout" />
                 </form>
             </div>
